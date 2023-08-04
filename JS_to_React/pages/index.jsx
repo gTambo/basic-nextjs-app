75e725ef7c4dd4584381f58a689e47e3cb77840a
Greg Tambornino <greg.tambornino@concordusa.com>
2023-08-04 15:15:43 -0500

    convert files to jsx for use with next

---
+++ JS_to_React/pages/index.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function Header({ title }) {
+  return <h1>{title ? title : "Default title"}</h1>;
+}
+
+function Article() {
+  return <article>A paragraph inside an article element.</article>;
+}
+
+export default function HomePage() {
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <Article />
+      <ul>
+        {names.map((name, idx) => (
+          <li key={name + idx}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Likes({likes})</button>
+    </div>
+  );
+}
